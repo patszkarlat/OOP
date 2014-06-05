91d97ed4ca066f9e76979b99718dfc1c5718b8ec
patszkarlat <punkgeek@mac.com>
2014-06-05 14:51:36 -0400

    done

--- people.rb
+++ people.rb
@@ -3,35 +3,35 @@ class Person
 
 	def initialize(name)
 		@name = name
-end
+	end
 
-def greeting
-	puts "Hi! My name is #{name}!"
-end
+	def greeting
+		puts "Hi! My name is #{name}!"
+	end
 end
 
 class Student < Person
 	def learn
 		puts "I get it!"
-end
+	end
 end
 
 class Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object."
 	end
-
 end
 
 instance1 = Instructor.new("Chris")
 instance2 = Student.new("Christina")
 
-puts instance1.greeting
-puts instance2.greeting
+instance1.greeting
+instance2.greeting
 
 instance1.teach
 instance2.learn
 
 instance2.teach
+#this doesn't work because student does not have the def 'teach', only instructor does.
 
 
